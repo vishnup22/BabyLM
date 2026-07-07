b618e7b3cc5cafc107a138d98cd35b3856ec98c4
vishnup22 <prohithrocks@gmail.com>
2026-07-07 16:20:55 -0400

    Fix SLURM: move logs to BabyLM/logs/ (no space), fix conda init path

--- monolingual eval/slurm_english.sh
+++ monolingual eval/slurm_english.sh
@@ -7,14 +7,12 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=64G
 #SBATCH --time=1-00:00:00
-#SBATCH --output=/nfs/storage1/home/pulipakv/BabyLM/monolingual eval/logs/eval_english_%j.out
-#SBATCH --error=/nfs/storage1/home/pulipakv/BabyLM/monolingual eval/logs/eval_english_%j.err
+#SBATCH --output=/nfs/storage1/home/pulipakv/BabyLM/logs/eval_english_%j.out
+#SBATCH --error=/nfs/storage1/home/pulipakv/BabyLM/logs/eval_english_%j.err
 
-source ~/.bashrc
+source /home/pulipakv/miniconda3/etc/profile.d/conda.sh
 conda activate telugu_llm
 
-WORKDIR="/nfs/storage1/home/pulipakv/BabyLM/monolingual eval"
-mkdir -p "$WORKDIR/logs"
-cd "$WORKDIR"
+cd "/nfs/storage1/home/pulipakv/BabyLM/monolingual eval"
 
 python english.py --cleaned_dir cleaned
